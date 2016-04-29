8d6b50750d2549ca23c7aed23de46baaf8d57732
Ben Norris <ben@bsn.io>
2016-04-28 19:23:35 -0600

    Remove memory warning function

--- SolarSystem/ViewController.swift
+++ SolarSystem/ViewController.swift
@@ -16,11 +16,6 @@ class ViewController: UIViewController {
         // A very important change
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
